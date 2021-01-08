d2761b35b2a79f1ca663055fde48ef0ba724b605
takuto <sg.epk.1231@gmail.com>
2021-01-08 13:54:37 +0900

    fix run-elasticsearch.sh

--- run-elasticsearch.sh
+++ run-elasticsearch.sh
@@ -19,32 +19,24 @@ fi
 
 docker network create elastic
 
-for (( node=1; node<=${NODES-1}; node++ ))
+NODES=${NODES-1}
+for (( node=1; node<=$NODES; node++ ))
 do
-  port_com=$((9300 + $node - 1))
-  UNICAST_HOSTS+="es$node:${port_com},"
-done
-
-for (( node=1; node<=${NODES-1}; node++ ))
-do
-  port=$((PORT + $node - 1))
-  port_com=$((9300 + $node - 1))
+  port=$((9300 + $node - 1))
   docker run \
     --rm \
     --env "node.name=es${node}" \
     --env "cluster.name=docker-elasticsearch" \
+    --env "cluster.initial_master_nodes=es1" \
+    --env "discovery.seed_hosts=es1" \
     --env "cluster.routing.allocation.disk.threshold_enabled=false" \
     --env "bootstrap.memory_lock=true" \
     --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
     --env "xpack.security.enabled=false" \
     --env "xpack.license.self_generated.type=basic" \
-    --env "discovery.zen.ping.unicast.hosts=${UNICAST_HOSTS}" \
-    --env "discovery.zen.minimum_master_nodes=${NODES}" \
-    --env "http.port=${port}" \
     --ulimit nofile=65536:65536 \
     --ulimit memlock=-1:-1 \
-    --publish "${port}:${port}" \
-    --publish "${port_com}:${port_com}" \
+    --publish "${port}:9200" \
     --detach \
     --network=elastic \
     --name="es${node}" \
@@ -63,7 +55,7 @@ docker run \
   --retry-connrefused \
   --show-error \
   --silent \
-  http://es1:$PORT
+  http://es1:9300
 
 sleep 10
 
