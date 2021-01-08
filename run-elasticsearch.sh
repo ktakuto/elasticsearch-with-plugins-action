44b1abb5caf3f3d7e57ef5e12290dd7acb399c47
takuto <sg.epk.1231@gmail.com>
2021-01-08 13:18:51 +0900

    fix all

--- run-elasticsearch.sh
+++ run-elasticsearch.sh
@@ -7,11 +7,10 @@ if [[ -z $STACK_VERSION ]]; then
   exit 1
 fi
 
-#
-PLUGIN_INSTAALL_CMD=""
+PLUGIN_INSTALL_CMD=""
 PLUGINS_STR=`echo ${PLUGINS} | sed -e 's/\n/ /g'`
 if [ -n "${PLUGINS_STR}" ]; then
-  ARRAY=(${PLUGIN_STR})
+  ARRAY=(${PLUGINS_STR})
   for i in "${ARRAY[@]}"
   do
     PLUGIN_INSTALL_CMD+="elasticsearch-plugin install --batch ${i} && "
@@ -20,60 +19,30 @@ fi
 
 docker network create elastic
 
-for (( node=1; node<=${NODES-1}; node++ ))
+NODES=${NODES-1}
+for (( node=1; node<=$NODES; node++ ))
 do
-  port_com=$((9300 + $node - 1))
-  UNICAST_HOSTS+="es$node:${port_com},"
-done
-
-for (( node=1; node<=${NODES-1}; node++ ))
-do
-  port=$((PORT + $node - 1))
-  port_com=$((9300 + $node - 1))
-  if [ "x${MAJOR_VERSION}" == 'x6' ]; then
-    docker run \
-      --rm \
-      --env "node.name=es${node}" \
-      --env "cluster.name=docker-elasticsearch" \
-      --env "cluster.routing.allocation.disk.threshold_enabled=false" \
-      --env "bootstrap.memory_lock=true" \
-      --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
-      --env "xpack.security.enabled=false" \
-      --env "xpack.license.self_generated.type=basic" \
-      --env "discovery.zen.ping.unicast.hosts=${UNICAST_HOSTS}" \
-      --env "discovery.zen.minimum_master_nodes=${NODES}" \
-      --env "http.port=${port}" \
-      --ulimit nofile=65536:65536 \
-      --ulimit memlock=-1:-1 \
-      --publish "${port}:${port}" \
-      --publish "${port_com}:${port_com}" \
-      --detach \
-      --network=elastic \
-      --name="es${node}" \
-      docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION} \
-      /bin/sh -vc "${PLUGIN_INSTALL_CMD} /usr/local/bin/docker-entrypoint.sh"
-  done
-
-    docker run \
-      --rm \
-      --env "node.name=es${node}" \
-      --env "cluster.name=docker-elasticsearch" \
-      --env "cluster.initial_master_nodes=es1" \
-      --env "discovery.seed_hosts=es1" \
-      --env "cluster.routing.allocation.disk.threshold_enabled=false" \
-      --env "bootstrap.memory_lock=true" \
-      --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
-      --env "xpack.security.enabled=false" \
-      --env "xpack.license.self_generated.type=basic" \
-      --env "http.port=${port}" \
-      --ulimit nofile=65536:65536 \
-      --ulimit memlock=-1:-1 \
-      --publish "${port}:${port}" \
-      --detach \
-      --network=elastic \
-      --name="es${node}" \
-      docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}
-  fi
+  port=$((9200 + $node - 1))
+  docker run \
+    --rm \
+    --env "node.name=es${node}" \
+    --env "cluster.name=docker-elasticsearch" \
+    --env "cluster.initial_master_nodes=es1" \
+    --env "discovery.seed_hosts=es1" \
+    --env "cluster.routing.allocation.disk.threshold_enabled=false" \
+    --env "bootstrap.memory_lock=true" \
+    --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
+    --env "xpack.security.enabled=false" \
+    --env "xpack.license.self_generated.type=basic" \
+    --ulimit nofile=65536:65536 \
+    --ulimit memlock=-1:-1 \
+    --publish "${port}:9200" \
+    --detach \
+    --network=elastic \
+    --name="es${node}" \
+    --entrypoint="" \
+    docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION} \
+    /bin/sh -vc "${PLUGIN_INSTALL_CMD} /usr/local/bin/docker-entrypoint.sh"
 done
 
 docker run \
@@ -86,7 +55,7 @@ docker run \
   --retry-connrefused \
   --show-error \
   --silent \
-  http://es1:$PORT
+  http://es1:9200
 
 sleep 10
 
